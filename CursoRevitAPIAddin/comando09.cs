2b3d6e03f42ccca2b6f740736106cdaf719ab433
gerald pachari <geraldpm123@hotmail.com>
2021-02-08 22:21:35 -0500

    Clase 09 Parte 2

---
+++ CursoRevitAPIAddin/comando09.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+
+namespace CursoRevitAPIAddin
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    class comando09 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Variables basicas que pueden servirnos de Revit
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            //Seleccion de curvas o lienas de modelo
+            List<Reference> sel = uiDoc.Selection.PickObjects(ObjectType.Element, "Seleccione lineas de modelo").ToList();
+
+            List<ModelCurve> curvas = new List<ModelCurve>();
+            foreach (Reference reference in sel)
+            {
+                ModelCurve curva = doc.GetElement(reference) as ModelCurve;
+                if (curva!=null)
+                {
+                    curvas.Add(curva);
+                }
+            }
+
+            formulario09CreacionFrames frm = new formulario09CreacionFrames(doc,curvas);
+            frm.ShowDialog();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ CursoRevitAPIAddin/formulario09CreacionFrames.cs
@@ -0,0 +1,54 @@
+﻿
+using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoRevitAPIAddin
+{
+    public partial class formulario09CreacionFrames : System.Windows.Forms.Form
+    {
+        Document _doc;
+        List<ModelCurve> _listaCurvas;
+        public formulario09CreacionFrames(Document doc,List<ModelCurve> listaCurvas )
+        {
+            InitializeComponent();
+            _doc = doc;
+            _listaCurvas = listaCurvas;
+            //Llenar comboBoxTipo
+            FilteredElementCollector col = new FilteredElementCollector(_doc);
+            col.OfCategory(BuiltInCategory.OST_StructuralFraming);
+            col.WhereElementIsElementType();
+            List<FamilySymbol> tipos = new List<FamilySymbol>();
+            foreach (Element element in col)
+            {
+                tipos.Add(element as FamilySymbol);
+            }
+            cmbTipos.DataSource = tipos;
+            cmbTipos.DisplayMember = "Name";
+            //Rellenar Niveles
+            cmbNiveles.DataSource = Tools.ObtenerListaNiveles(_doc);
+            cmbNiveles.DisplayMember = "Name";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FamilySymbol tipo = cmbTipos.SelectedItem as FamilySymbol;
+            Level nivel = cmbNiveles.SelectedItem as Level;
+            Transaction t = new Transaction(_doc, "Creacion de Frames");
+            t.Start();
+            foreach (ModelCurve curve in _listaCurvas)
+            {
+                LocationCurve cc = curve.Location as LocationCurve;
+                _doc.Create.NewFamilyInstance(cc.Curve, tipo, nivel, Autodesk.Revit.DB.Structure.StructuralType.Beam);
+            } 
+            t.Commit();
+        }
+    }
+}
